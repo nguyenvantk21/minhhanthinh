ca15b56918e4fa135d580ba00d42e03f900f530d
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-06-27 08:53:54 +0700

    push 1

---
+++ wp-content/themes/mht-selftheme/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <style type="text/css">
+        html {width: 100%; background-color: grey; height: 500px;}
+    </style>
+</head>
+<body>
+<div class="head"></div>ok
